a595205db154f89afbab91c83f2d5d65b7bcda94
Elitetykoro <tykoromathijssen@gmail.com>
2024-09-13 12:39:13 +0200

    opdracht 1- 3

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,31 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject prefab;
     private int x,y,z;
+    float r,g,b;
+    Color randColor;
     void Start()
     {
         for (int i = 0; i < 100; i++)
         {
-            //Color color = RandomColor();
-            //Vector3 randPos = RandomPosition(-10f, 10f);
-            //CreateBall(color, randPos);
+            Color color = RandomColor();
+            Vector3 randPos = RandomPosition(-10f, 10f);
+            CreateBall(color, randPos);
         }
     }
-    private void CreateBall(Color c,Vector3 pos)
+    private Color RandomColor()
+    {
+        r = Random.Range(0f, 1f);
+        g = Random.Range(0f, 1f);
+        b = Random.Range(0f, 1f);
+        return new Color(r, g, b);
+    }
+    private Vector3 RandomPosition(float x,float y)
+    {
+        Vector3 RandomPosition = new Vector3(Random.Range(x, y), Random.Range(x, y), Random.Range(x, y));
+        return RandomPosition;
+    }
+    private GameObject CreateBall(Color c,Vector3 pos)
     {
         GameObject ball = Instantiate(prefab, pos, Quaternion.identity);
-        Material material = ball.GetComponent<Material>();
-        material.SetColor("_Color", c);
+        Material material = ball.GetComponent<MeshRenderer>().material;
+        if (material.shader.name == "Universal Render Pipeline/Lit") material.SetColor("_BaseColor", c);
+        else Debug.LogWarning("Pleeg Zelfmoord");
         StartCoroutine(DestroyBall(ball));
+        return ball;
     }
     private IEnumerator DestroyBall(GameObject destroyObject)
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(3);
         DestroyBall(destroyObject);
 
     }
@@ -36,10 +52,10 @@ public class BallScript : MonoBehaviour
         elapsedTime += Time.deltaTime;
         if (elapsedTime > 1f)
         {
-            //Color color = RandomColor();
-            //Vector3 randPos = RandomPosition(-10f, 10f);
-            //GameObject ball = CreateBall(color, randPos);
-            //DestroyBall(ball);
+            Vector3 randPos = RandomPosition(-5,5);
+            Color randColor = RandomColor();
+            GameObject ball = CreateBall(randColor, randPos);
+            DestroyBall(ball);
             elapsedTime = 0f;
         }
     }
